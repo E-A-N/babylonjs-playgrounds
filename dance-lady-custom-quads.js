dcfd8adedc4eeee57ece7457b3ac5a838301c8a4
Cyborg_Ean <ean.director@gmail.com>
2025-07-28 10:12:32 -0700

    Update dance-lady-custom-quads.js

--- dance-lady-custom-quads.js
+++ dance-lady-custom-quads.js
@@ -1,95 +1,3 @@
-/*
-var createScene = function () {
-    var scene = new BABYLON.Scene(engine);
-
-    // Camera
-    var camera1 = new BABYLON.ArcRotateCamera("camera1", Math.PI / 2, Math.PI / 4, 10, new BABYLON.Vector3(0, -5, 0), scene);
-    scene.activeCamera = camera1;
-    scene.activeCamera.attachControl(canvas, true);
-    camera1.lowerRadiusLimit = 2;
-    camera1.upperRadiusLimit = 10;
-    camera1.wheelDeltaPercentage = 0.01;
-
-    // Lighting
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 0.6;
-    light.specular = BABYLON.Color3.Black();
-
-    var light2 = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, -1.0), scene);
-    light2.position = new BABYLON.Vector3(0, 5, 5);
-
-    // Ground
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", { height: 50, width: 50, subdivisions: 4 }, scene);
-    var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-    groundMaterial.diffuseColor = new BABYLON.Color3(0.3, 0.3, 0.3);
-    groundMaterial.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
-    ground.material = groundMaterial;
-
-    // Load the animated GLB model
-    BABYLON.ImportMeshAsync("https://assets.babylonjs.com/meshes/HVGirl.glb", scene).then(function (result) {
-        const root = result.meshes[0];
-        // Loop through all meshes with geometry
-        let quads = 0;
-        
-        result.meshes.forEach(mesh => {
-            if (!(mesh instanceof BABYLON.Mesh) || mesh.getTotalVertices() === 0) return;
-            mesh.convertToUnIndexedMesh();
-
-            // Set up a material that supports vertex colors
-            const mat = new BABYLON.StandardMaterial("mat", scene);
-            mat.diffuseColor = new BABYLON.Color3(1, 1, 1);
-            // mat.specularColor = new BABYLON.Color3(0, 0, 0);
-            mat.vertexColors = true;
-
-            mesh.material = mat;
-
-            // Extract vertex data
-            const vertexData = BABYLON.VertexData.ExtractFromMesh(mesh, true);
-            if (!vertexData.positions || vertexData.positions.length === 0) return;
-
-            const numTriangles = vertexData.positions.length / 9; // 3 vertices per triangle, 3 floats per vertex
-            const colors = [];
-
-            for (let tri = 0; tri < numTriangles; tri += 2) {
-                quads++;
-                const color = new BABYLON.Color4(Math.random(), Math.random(), Math.random(), 1.0);
-
-                // Each triangle has 3 vertices
-                for (let i = 0; i < 6; i++) {
-                    colors.push(color.r, color.g, color.b, color.a);
-                }
-            }
-
-            vertexData.colors = colors;
-            vertexData.applyToMesh(mesh, true);
-        });
-
-        console.log("total meshes/quads:", result.meshes.length, quads);
-        
-
-        // Scale the whole model
-        root.scaling.scaleInPlace(0.1);
-
-        // Point camera at the model
-        camera1.target = root;
-
-        // Play animation
-        const sambaAnim = scene.getAnimationGroupByName("WalkingBack");
-        sambaAnim?.start(true, 1.0, sambaAnim.from, sambaAnim.to, false);
-        console.log(scene.animationGroups);
-    })
-    .then(() => {
-        setTimeout(() => {
-            const sceneInstru = new BABYLON.SceneInstrumentation(scene);
-            console.log("Drawcalls:", sceneInstru.drawCallsCounter.current);
-        }, 5000)
-    })
-
-
-    return scene;
-};
-
-*/
 var delayCreateScene = function () {
     var scene = new BABYLON.Scene(engine);
     let nodeNames = [];
@@ -99,19 +7,17 @@ var delayCreateScene = function () {
 
         for (var node of entries.rootNodes) {
             node.position.x += offset;
-            console.log("eandebug node is:", node.name);
         }
+
+        let characterMeshes = [];
 
         for (let n in nodeNames){
             let name = nodeNames[n];
             let actualName = "Clone of " + name;
             let mesh = scene.getMeshByName(actualName);
             mesh.name = `lady${idNum}-${n}`;
-            console.log("clone mesh:", mesh.name)
+            characterMeshes.push(mesh);
         }
-
-        // const cloneMesh = scene.getMeshByName("Clone of Body");
-        // console.log("eandebug clonemesh is:", cloneMesh);
 
         let animationOptions = Object.keys(entries.animationGroups);
         let animationChoice = Math.floor(
@@ -125,6 +31,38 @@ var delayCreateScene = function () {
         setTimeout(() => {
             animationGroup.play(true);
         }, delay);
+
+        //color the vertices
+        let quads = 0;
+        if (idNum > 0){
+            return;
+        }
+        for (let i in characterMeshes){
+            let mesh = characterMeshes[i];
+            mesh.convertToUnIndexedMesh();
+            mesh.useVertexColors = true;
+
+            const vertexData = BABYLON.VertexData.ExtractFromMesh(mesh, true);
+            if (vertexData.positions && vertexData.positions.length > 0) {
+                const numTriangles = vertexData.positions.length / 9; // 3 vertices per triangle, 3 floats per vertex
+                const colors = [];
+
+                for (let tri = 0; tri < numTriangles; tri += 2) {
+                    let color = new BABYLON.Color4(0, 0, 0, 1.0);
+                    quads++;
+                    if(quads > 100 && quads < 400){
+                        color = new BABYLON.Color4(Math.random(), Math.random(), Math.random(), 1.0);
+                    }
+                    // Each triangle has 3 vertices
+                    for (let i = 0; i < 6; i++) {
+                        colors.push(color.r, color.g, color.b, color.a);
+                    }
+                }
+                vertexData.colors = colors;
+                vertexData.applyToMesh(mesh, true);
+                mesh.optimizeIndices();
+            }
+        }
     }
     
     BABYLON.loadAssetContainerAsync("https://assets.babylonjs.com/meshes/HVGirl.glb", scene).then(function (container) {
